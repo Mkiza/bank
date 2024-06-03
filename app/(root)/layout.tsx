819667e4091964af7c118960311842e130d3f723
“Mkiza” <ronaldbagenda@gmail.com>
2024-06-03 18:32:24 +0200

    minor changes

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,42 +1,17 @@
 import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  /*$id: string;
-  email: string;
-  userId: string;
-  dwollaCustomerUrl: string;
-  dwollaCustomerId: string;
-  firstName: string;
-  lastName: string;
-  address1: string;
-  city: string;
-  state: string;
-  postalCode: string;
-  dateOfBirth: string;
-  ssn: string;
-*/
-  const loggedIn = {
-    firstName: "Ronald",
-    lastName: "Bagenda",
-    email: "test@gmail.com",
-    $id: "1",
-    userId: "1",
-    dwollaCustomerUrl: "1",
-    dwollaCustomerId: "1",
-    address1: "1",
-    city: "1",
-    state: "1",
-    postalCode: "1",
-    dateOfBirth: "1",
-    ssn: "1",
-    name: "Ronald Bagenda", // Add the 'name' property
-  };
+  const loggedIn = await getLoggedInUser();
+
+  if (!loggedIn) redirect("/sign-in");
 
   return (
     <main className="flex h-screen w-full font-inter">
